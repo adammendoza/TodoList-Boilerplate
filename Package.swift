daa847cdb15ecd414a583fa2528032ede29d06fe
king33 <king@us.ibm.com>
2016-11-11 12:25:08 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     ],
     dependencies: [
          .Package(url: "https://github.com/IBM-Swift/Kitura.git",           majorVersion: 1, minor: 1),
-         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",     majorVersion: 1, minor: 1),
-         .Package(url: "https://github.com/rfdickerson/Swift-Kuery-PostgreSQL", majorVersion: 0)
+         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",     majorVersion: 1, minor: 1)
     ]
 )
